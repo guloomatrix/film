3b1c4678ab356d5f09eeaee12eaf9572657a52d2
Gul Yanik <melleyanik@hotmail.com>
2023-07-12 12:38:19 +0200

    cinemaweb

---
+++ moovies.php
@@ -0,0 +1,155 @@
+<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/Matrix.jpg" alt="Image training day" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Matrix</h2>
+                        <p class="card-text">Programmeur anonyme dans un service administratif le jour, Thomas Anderson devient Neo la nuit venue. Sous ce pseudonyme, il est l'un des pirates les plus recherchés du cyber-espace. A cheval entre deux mondes, Neo est assailli par d'étranges songes et des messages cryptés provenant d'un certain Morpheus. Celui-ci l'exhorte à aller au-delà des apparences et à trouver la réponse à la question qui hante constamment ses pensées : qu'est-ce que la Matrice ? Nul ne le sait, et aucun homme n'est encore parvenu à en percer les defenses. Mais Morpheus est persuadé que Neo est l'Elu, le libérateur mythique de l'humanité annoncé selon la prophétie. Ensemble, ils se lancent dans une lutte sans retour contre la Matrice et ses terribles agents...</p><br>
+                    <strong>Acteur principal : Denzel WashingtonEthan HawkeScott Glenn</strong>
+                    <p class="card-text"><small class="text-muted">Date de sortie: 24 mai 1999 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/shaolinsoccer.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">shaolinsoccer</h2>
+                        <p class="card-text">Fung, une ancienne gloire du football, se tourne vers Sing, un moine Shaolin qui possède des dons extraordinaires en matière d'arts martiaux. Avec son aide, il espère créer une équipe de foot Shaolin capable de vaincre ses adversaires...</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 21 Aout 2002 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/americanbeauty.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                       <h2 class="card-title">American Beauty</h2>
+                        <p class="card-text">Un père de banlieue sexuellement frustré entre en pleine crise de la quarantaine après s'être épris de la meilleure amie de sa fille.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 2 février 2000 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/forestgump.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">forestgump</h2>
+                        <p class="card-text">Quelques décennies d'histoire américaine, des années 1940 à la fin du XXème siècle, à travers le regard et l'étrange odyssée d'un homme simple et pur, Forrest Gump.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 5 octobre 1994 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>  
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/avengers.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">avengers</h2>
+                        <p class="card-text">En 1980 à Miami, un immigrant cubain déterminé prend en charge un cartel de drogue et succombe au vice d'avidité.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 20 avril 2012 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/operation-dragon_2.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title"><operation-dragon_2></operation-dragon_2></h2>
+                        <p class="card-text">Une compétition d'arts martiaux est organisée par un mystérieux Han, derrière lequel se cache un redoutable trafiquant de drogue. Lee, agent de renseignements occidental et spécialiste de kung-fu, tente de démanteler l'organisation criminelle.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 30 janvier 1974 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/Alita.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Alita</h2>
+                        <p class="card-text">Lorsqu’Alita se réveille sans aucun souvenir de qui elle est, dans un futur qu’elle ne reconnaît pas, elle est accueillie par Ido, un médecin qui comprend que derrière ce corps de cyborg abandonné, se cache une jeune femme au passé extraordinaire. Ce n’est que lorsque les forces dangereuses et corrompues qui gèrent la ville d’Iron City se lancent à sa poursuite qu’Alita découvre la clé de son passé...</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 13 fevrier 2019 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/taichi.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">taichi</h2>
+                        <p class="card-text">Chine, 19ème siècle, une guerre sans fin ravage le royaume. Sur le champs de bataille, un guerrier d’exception fait preuve d’une force sans limite. Mais ce don surhumain finira aussi par le tuer.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 3 juillet 2013 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mr-3" style="max-width:540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="filmsforsite/Nikita.jpg" alt="" class="card-img" style="height:35vh; object-fit: cover;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Nikita</h2>
+                        <p class="card-text">
+Ex-criminelle devenue agent secret au sein d'une organisation mystérieuse, Nikita se retourne contre ses employeurs, quand ceux-ci font assassiner son bien-aimé. Sa mission dès lors : détruire cette Division qui transforme des jeunes désorientés en machine à tuer pour servir d’obscurs desseins.</p>
+                        <p class="card-text"><small class="text-muted">Date de sortie: 21 févriere 1990 (France)</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+       
+    </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ shaolinsoccer.php
@@ -0,0 +1,86 @@
+<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.theme.min.css">
+    <link rel="stylesheet" href="films.css">
+</head>
+<body>
+
+    <div class="conatainer-fluid">
+        <h1 style="text-align:center;">Shaolin soccer</h1>
+        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <img src="filmsforsite/shaolinsoccer.jpg" alt="affiche film training day" class="img-fluid" style="max-width:100%" width="300px;" height="600px;">
+                        <img src="filmsforsite/shaolinsoccer.jpg" alt="affiche film training day" class="img-fluid" style="max-width:100%" width="300px;" height="600px;">
+                        <img src="filmsforsite/shaolinsoccer.jpg" alt="affiche film training day" class="img-fluid" style="max-width:100%" width="300px;" height="600px;">
+                        <img src="filmsforsite/Shaolinsoccer.jpg" alt="affiche film training day" class="img-fluid" style="max-width:100%" width="300px;" height="600px;">
+                    </div>
+                    <div class="col">
+                    Programmeur anonyme dans un service administratif le jour, Thomas Anderson devient Neo la nuit venue. Sous ce pseudonyme, il est l'un des pirates les plus recherchés du cyber-espace. A cheval entre deux mondes, Neo est assailli par d'étranges songes et des messages cryptés provenant d'un certain Morpheus. Celui-ci l'exhorte à aller au-delà des apparences et à trouver la réponse à la question qui hante constamment ses pensées : qu'est-ce que la Matrice ? Nul ne le sait, et aucun homme n'est encore parvenu à en percer les defenses. Mais Morpheus est persuadé que Neo est l'Elu, le libérateur mythique de l'humanité annoncé selon la prophétie. Ensemble, ils se lancent dans une lutte sans retour contre la Matrice et ses terribles agents...<br>
+                    <strong>Acteur principal : Denzel WashingtonEthan HawkeScott Glenn</strong>
+                    </div>
+                </div>
+            </div>
+            <div class="glide" style="margin-top:450px;">
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                    <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/americanbeauty.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="Matrix.php"><img src="filmsforsite/Matrix.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/operation-dragon_2.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="shaolinsoccer.php"><img src="filmsforsite/shaolinsoccer.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/taichi.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/forestgump.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="Nikita.php"><img src="filmsforsite/Nikita.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/avengers.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+                <li class="glide__slide" style="text-align:center;"><a href="#"><img src="filmsforsite/Alita.jpg" width="200px;" height="200px;" style="border-radius: 10px"></a></li>
+            </ul>
+                </div>
+            <div class="glide__arrows" data-glide-el="controls">
+                <button class="glide__arrow glide__arrow--left" data-glide-dir="<"><i class="fa-solid fa-arrow-left"></i></button>
+                <button class="glide__arrow glide__arrow--right" data-glide-dir=">"><i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+        </div>
+
+     </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+<script>
+    const myVar = {
+
+        type: 'carousel',
+        perView: 4,
+        breakpoints: {
+            1100: {
+            perView: 3
+            },
+            1000: {
+            perView: 3
+            },
+            900: {
+            perView: 3
+            },
+            800: {
+            perView: 2
+            },
+            600: {
+            perView: 1
+            }
+
+        }
+    }
+  new Glide('.glide',myVar).mount()
+</script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
